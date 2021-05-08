12c1acb1cc4c245b48a4815fa081b061267a5e01
Artem <toma200111@gmail.com>
2021-05-08 05:12:32 +0300

    Initial commit

---
+++ zmeyka/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zmeyka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("hello w0rld");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
